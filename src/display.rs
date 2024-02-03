99e4e9f0081cd3ae59dc67bf85cc726d8a1a9869
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2024-02-03 08:23:44 -0600

    Implement Display for display

--- src/display.rs
+++ src/display.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use std::fmt;
 use thiserror::Error;
 
 type Bit = bool;
@@ -8,33 +9,36 @@ const COLUMNS: usize = 64;
 const ROWS: usize = 32;
 
 
+#[derive(Clone, Copy, Debug)]
 pub struct PixelLocation {
-    x: usize,
-    y: usize,
+    pub x: usize,
+    pub y: usize,
 }
 
 impl PixelLocation {
     pub fn to_array_index(&self) -> usize {
-        self.x + (self.y * COLUMNS)
+        let wrapped_y = self.y % ROWS;
+        let wrapped_x = self.x % COLUMNS;
+
+        wrapped_x + (wrapped_y * COLUMNS)
     }
 }
 
-pub struct Display {
-    state: [Bit; COLUMNS * ROWS],
+pub struct Chip8Display {
+    pixels: [Bit; COLUMNS * ROWS],
 }
 
-
-impl Display {
+impl Chip8Display {
     pub fn new() -> Self {
-        Display {
-            state: [false as Bit; COLUMNS * ROWS]
+        Chip8Display {
+            pixels: [false as Bit; COLUMNS * ROWS]
         }
     }
 
     /// Set the provided pixel to value.
     pub fn set_pixel(&mut self, pixel: PixelLocation,value: Bit) -> Result<(), ()> {
         let idx = pixel.to_array_index();
-        let pixel = &mut self.state[idx];
+        let pixel = &mut self.pixels[idx];
 
         *pixel = value;
 
@@ -42,19 +46,41 @@ impl Display {
     }
 
     pub fn get_state(&self) -> &[Bit] {
-        &self.state
+        &self.pixels
     }
 
     /// Return a mutable reference to the state of the display.
     pub fn get_state_mut(&mut self) -> &mut [Bit] {
-        &mut self.state
+        &mut self.pixels
     }
 
     /// Return the value (on or off) of the pixel at the provided point.
-    pub fn pixel_at(&self, point: PixelLocation) -> Bit {
-        // TODO - Bounds-check the point's x & y
+    pub fn pixel_at_location(&self, point: PixelLocation) -> Bit {
         let idx = point.to_array_index();
-        self.state[idx]
+        self.pixels[idx]
+    }
+}
+
+impl fmt::Display for Chip8Display {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in 0..ROWS {
+            for col in 0..COLUMNS {
+                let idx = row * COLUMNS + col;
+
+                // TODO - Verify correctness
+                let char = if self.pixels[idx] {
+                    "■"
+                } else {
+                    " "
+                };
+
+                write!(f, "{} ", char)?;
+            }
+
+            writeln!(f)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -69,25 +95,56 @@ mod tests {
 
     #[test]
     fn set_pixel_in_bounds_updates_display_state() {
-        let mut d = Display::new();
+        let mut d = Chip8Display::new();
 
         let origin = PixelLocation {
             x: 0,
             y: 0,
         };
         
-        assert_eq!(d.get_state()[0], false); // All pixels are initially "off" (false)
+        assert_eq!(d.get_state()[origin.to_array_index()], false); // All pixels are initially "off" (false)
         assert_eq!(d.set_pixel(origin, true), Ok(())); // Updating valid pixel should succeed
-        assert_eq!(d.get_state()[0], true); // Pixel we set should now be "on" (true)
+        assert_eq!(d.get_state()[origin.to_array_index()], true); // Pixel we set should now be "on" (true)
         
         let middle_of_screen = PixelLocation {
             x: 54,
             y: 28
         };
 
-        assert_eq!(d.get_state()[0], false); // All pixels are initially "off" (false)
+        assert_eq!(d.get_state()[middle_of_screen.to_array_index()], false); // All pixels are initially "off" (false)
         assert_eq!(d.set_pixel(middle_of_screen, true), Ok(())); // Updating valid pixel should succeed
-        assert_eq!(d.get_state()[0], true); // Pixel we set should now be "on" (true)
+        assert_eq!(d.get_state()[middle_of_screen.to_array_index()], true); // Pixel we set should now be "on" (true)
+    }
+
+    #[test]
+    fn set_pixel_out_of_bounds_wraps_to_in_bounds() {
+        let mut d = Chip8Display::new();
+
+        let oob_1 = PixelLocation {
+            x: 129, // Max is 64
+            y: 64   // Max is 32
+        };
+
+        assert_eq!(oob_1.to_array_index(), 1); // Array index should wrap around
+
+        assert_eq!(d.get_state()[oob_1.to_array_index()], false); // All pixels are initially "off" (false)
+        assert_eq!(d.pixel_at_location(oob_1), false);
+        assert_eq!(d.set_pixel(oob_1, true), Ok(())); // Updating oob pixel should succeed
+        assert_eq!(d.get_state()[oob_1.to_array_index()], true); // Pixel we set should now be "on" (true)
+        assert_eq!(d.pixel_at_location(oob_1), true);
+        
+        let oob_2 = PixelLocation {
+            x: 129,
+            y: 66
+        };
+
+        assert_eq!(oob_2.to_array_index(), 129); // Array index should wrap around
+
+        assert_eq!(d.get_state()[oob_2.to_array_index()], false); // All pixels are initially "off" (false)
+        assert_eq!(d.pixel_at_location(oob_2), false);
+        assert_eq!(d.set_pixel(oob_2, true), Ok(())); // Updating oob pixel should succeed
+        assert_eq!(d.get_state()[oob_2.to_array_index()], true); // Pixel we set should now be "on" (true)
+        assert_eq!(d.pixel_at_location(oob_2), true);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use display::*;
+
 mod display;
 
 fn main() {
-    println!("Hello, world!");
+    let mut display = Chip8Display::new();
+
+    display.set_pixel(PixelLocation { x: 0, y: 0 }, true).expect("How tf did this cause an error");
+    display.set_pixel(PixelLocation { x: 12, y: 12}, true).expect("How tf did this cause an error");
+
+    println!("{}", display);
 }
 
